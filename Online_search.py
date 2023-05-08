ee8c808b244c4fbf4e6d210b4d6729bbba5d1000
Praveenmittakadapala8794 <mittakadapala.praveen@gmail.com>
2023-05-08 12:23:42 +0530

    Anything search in Online websites in this Project

---
+++ Online_search.py
@@ -0,0 +1,16 @@
+import pywhatkit as Praveen
+
+
+#play to songs in youtube
+
+# Praveen.playonyt("chamkeela angeelesi")
+
+#search in chrome
+
+# Praveen.search("software jobs")
+
+#getting information
+
+Praveen.info("cricket")
+
+
